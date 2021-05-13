5aae6e6997683df59098ecc04c3d0adaac52a1b2
Madhour <aymanmadhour@gmail.com>
2021-05-14 00:45:18 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data and sentiment analysis  on vaccine-acceptance by regions',
+    author='Ayman Madhour',
+    license='MIT',
+)
